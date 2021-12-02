c4e85ecdd09b4a75ab81c9c08b18f9f819295d26
Shipu8644 <shipu.monjurur@gmail.com>
2021-12-02 22:09:50 +0600

    useparams getting single prd

--- src/pages/Home/Product/Details.js
+++ src/pages/Home/Product/Details.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Details = () => {
-    const id = useParams();
-    console.log(id);
+    const { id } = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, [id])
+    console.log(product);
+
     return (
         <div>
-            <h1>Hello From details</h1>
+            <h1>Hello From details {product.name}</h1>
         </div>
     );
 };
