116c663d502dacd04799d4329150d2f8fc67ae80
Shipu8644 <shipu.monjurur@gmail.com>
2021-12-02 22:53:57 +0600

    details done

--- src/pages/Home/Product/Details.js
+++ src/pages/Home/Product/Details.js
@@ -1,4 +1,6 @@
+
 import React, { useEffect, useState } from 'react';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 const Details = () => {
@@ -10,12 +12,37 @@ const Details = () => {
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [id])
-    console.log(product);
+    const { name, img, seller, price, stock, star, category, _id,
+        starCount } = product;
 
     return (
-        <div>
-            <h1>Hello From details {product.name}</h1>
-        </div>
+        <Container className="mt-5 ">
+            <Row md={2} >
+                <Col>
+                    <img style={{ width: '350px' }} src={img} alt="" />
+                    <br />
+                    <br />
+                    <h5>{category}</h5>
+
+                </Col>
+                <Col style={{ textAlign: 'left' }} >
+                    <h5 style={{ color: 'blue' }}>{name}</h5>
+                    <p>Price: $<strong style={{ color: 'red' }}>{price}</strong></p>
+                    <p>viewd by {starCount} people</p>
+                    <p>Seller: {seller}</p>
+                    <p>Stock: {stock} left</p>
+                    <hr />
+                    <div className="m-2 p-2">
+                        <Button>Update this product</Button>
+                        <Button className="ms-5">Remove this product</Button>
+
+                    </div>
+                    <br />
+                    <Button className="m-4 p-2">View All Products</Button>
+
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
